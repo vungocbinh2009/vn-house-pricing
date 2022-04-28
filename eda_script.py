1d3bcfff1a77e22e2ca02a232aeb2a5cfabea93d
binh <vungocbinh@protonmail.com>
2022-04-28 22:22:36 +0700

    A lot of updates

--- eda_script.py
+++ eda_script.py
@@ -1,16 +1,10 @@
 # Đây là script để chạy d-tale: Đây là phần mềm giúp việc eda dữ liệu đơn giản hơn
 # Trước khi chạy, anh cần cài dtale
+# Sau đó anh copy đoạn code trong file này để dùng dtale để EDA - Sẽ dễ hơn.
+
 import pandas
 import dtale
-import pprint
-
 df = pandas.read_csv("VN_housing_dataset.csv")
-unique_districts = df["Quận"].unique()
-ward_by_district = {}
-for district in unique_districts:
-    district_data = df[df["Quận"] == district]
-    ward_by_district[district] = district_data["Huyện"].unique()
-pprint.pprint(ward_by_district)
 d = dtale.show(df)
 d.open_browser()
 
